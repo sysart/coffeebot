3c7b979bea80632a7a29249bbae5f29d2d7e6122
Sebastian Falk <sebastian.falk@sysart.fi>
2018-04-24 14:41:58 +0300

    Fixed slack messages

--- lambda-process-coffee-event/slack_messages.py
+++ lambda-process-coffee-event/slack_messages.py
@@ -1,7 +1,7 @@
 slack_messages = {
     'brew_started': 'Juhuu, uutta kahvia laitettu tippumaan. :coffee: :hourglass_flowing_sand: Kerron kohta kun on valmista :simple_smile:',
-    'brew_ended': 'Kahvikeitin juuri sammutettiin :feel1:',
-    'brew_timeout': 'Kahvikeitin sammui. Olisiko sitä vielä vähän jäljellä? :thinking_face:',
+    'brew_ended': 'Kahvinkeitin sammutettiin juuri :feel1:',
+    'brew_timeout': 'Kahvinkeitin sammui. Olisiko sitä vielä vähän jäljellä? :thinking_face:',
     'coffee_ready': '<!here>: Kahvi on valmis :coffee: :coffee: :coffee:',
     'out_of_coffee': '<!here>: Kahvi loppui!!! :scream: :scream_cat: :feelsgood:'
 }
